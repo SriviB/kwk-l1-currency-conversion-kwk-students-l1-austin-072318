b64274f552fff9db3fb67820f5cb9af3b28244f5
SriviB <srivibalaji33@gmail.com>
2018-07-27 14:22:47 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -2,12 +2,12 @@
 
 def usd_to_eur(dollar)
   euro = dollar * 0.86
-  "#{dollar} dollars is #{euro.round} euros."
+  "#{dollar} dollars is #{euro.floor} euros."
 end
 
 def eur_to_usd(euro)
   dollar = euro * 1.17
-  "#{euro} euros is #{dollar.round} dollars."
+  "#{euro} euros is #{dollar.floor} dollars."
 end
 
 puts usd_to_eur(5)
